d83cdc0061f091b81806b4cf6c5c0548e3b39e41
xyinshi2010 <1228187821@qq.com>
2026-04-26 22:36:37 +0800

    新建了一个M有Class类，增加了几个属性

---
+++ WpfApp3/MyClass.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    public   class MyClass
+    {
+        public Color Mycolor  { get; set; }
+        public int Age  { get; set; }
+        public string  MyName  { get; set; }
+        public double  Demycel  { get; set; }
+    }
+}
